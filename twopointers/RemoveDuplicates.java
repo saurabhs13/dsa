8bf15624d08eced0be73aacff15f6fd9b0db75bc
Saurabh <saurabhs13@gmail.com>
2024-07-23 11:32:23 +0530

    Removed duplicates'

---
+++ twopointers/RemoveDuplicates.java
@@ -0,0 +1,16 @@
+public class RemoveDuplicates{
+    public static int removeDuplicates(int[] arr){
+        int nextNonDuplicate = 1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[nextNonDuplicate-1] != arr[i]){
+                arr[nextNonDuplicate] = arr[i];
+                nextNonDuplicate++;
+            }
+        }
+        return nextNonDuplicate;
+    }
+    public static void main(String[] args){
+        int[] arr = new int[]{2,3,3,4,5,6,6,7,8,9,9,9,9,9,10};
+        System.out.println(removeDuplicates(arr));
+    }
+}
