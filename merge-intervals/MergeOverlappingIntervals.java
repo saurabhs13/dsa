938a77ec3df9d9b8671f46a2d731c8deeb0184ac
Saurabh <saurabhs13@gmail.com>
2025-07-01 09:50:14 +0530

    Updated array references

--- merge-intervals/MergeOverlappingIntervals.java
+++ merge-intervals/MergeOverlappingIntervals.java
@@ -7,13 +7,13 @@ public class MergeOverlappingIntervals {
     public static int[][] mergeIntervals(int[][] intervals){
         LinkedList<int[]> output = new LinkedList<>();
         Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
-        output.add(intervals[0]);
+        output.add(new int[]{intervals[0][0],intervals[0][1]});
         for(int i=1;i<intervals.length;i++){
             if(intervals[i][0]<=output.getLast()[1]){
                 if(intervals[i][1]>output.getLast()[1])
                     output.getLast()[1] = intervals[i][1];
             }else{
-                output.addLast(intervals[i]);
+                output.addLast(new int[]{intervals[i][0],intervals[i][1]});
             }
         }
         return output.toArray(new int[output.size()][]);
